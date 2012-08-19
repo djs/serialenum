4e91f7c532733c4415033ee12230d3bb6f402792
Dan Savilonis <djs@n-cube.org>
2012-08-19 02:19:49 -0400

    Update for pypi distribution
     * Add license
     * Add docs and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+"""
+serialenum
+----------
+
+A cross-platform serial port enumerator utility function. Use with pyserial to
+automatically detect available serial ports on any system.
+
+For more information, see the `github page <https://github.com/djs/serialenum>`_.
+"""
+
+
 from setuptools import setup, find_packages
 
 setup(
@@ -6,8 +17,19 @@ setup(
     py_modules = ['serialenum'],
     author = "Dan Savilonis",
     author_email = "djs@n-cube.org",
+    url = 'https://github.com/djs/serialenum',
     description = "Cross-platform serial port enumeration",
-    license = "PSF",
+    license = "BSD",
     keywords = "serial serial-port",
-    zip_safe=False
+    long_description = __doc__,
+    zip_safe=False,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
 )
